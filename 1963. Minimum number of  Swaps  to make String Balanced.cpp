614dcf176a51aac0de6708ab1e30fcab2afc7447
SjxSubham <sjxsubham@gmail.com>
2024-10-09 01:16:59 +0530

    1963. Minimum number of  Swaps  to make String Balanced.cpp

---
+++ 1963. Minimum number of  Swaps  to make String Balanced.cpp
@@ -0,0 +1,18 @@
+// ************************************* C++ *************************************
+
+T.C - O(n)
+
+  
+    int minSwaps(string s) {
+        int size = 0;
+        
+        for(char &ch : s) {
+            if(ch == '[')      
+              size++;
+            else if(size != 0)
+              size--;
+        }
+        
+        return (size+1)/2;
+    }
+};
