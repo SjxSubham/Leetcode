49c4a8954359aed2b189884682987e2b301ab48b
ANAS727189 <anas23khan083@gmail.com>
2025-10-02 01:42:09 +0530

    feat: added solution for problem no. 2785

---
+++ 2785.Sort Vowels in a String.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i' 
+            || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
+    }
+    
+    string sortVowels(string s) {
+        unordered_map<char, int> mp;
+        for (char c : s) {
+            if (isVowel(c)) {
+                mp[c]++;
+            }
+        }
+        string total_vow = "AEIOUaeiou", ans;
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (!isVowel(s[i])) ans += s[i];
+            else {
+                while(mp[total_vow[j]] == 0) j++;
+                ans += total_vow[j];
+                mp[total_vow[j]]--;
+            }
+        }
+        return ans;
+    }
+};
