eef1c741f6094f445c7997c93de3eb519202c88f
Tannu Singh <you@example.com>
2025-10-20 15:06:41 +0530

    Rename file to match PR name

--- 99.Maximize the Number of Target Nodes After Connecting Trees.cpp
+++ 99.Maximize the Number of Target Nodes After Connecting Trees.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
