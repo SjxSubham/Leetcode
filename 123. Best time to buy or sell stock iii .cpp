e569d71db1d0a93c8ddd045554594b9fb0ac6e19
aastikdas <aastikdas126@gmail.com>
2025-10-22 08:14:02 +0530

    modified 123.Best time to buy or sell stock iii.cpp

--- 123. Best time to buy or sell stock iii .cpp
+++ 123. Best time to buy or sell stock iii .cpp
@@ -1,62 +1,24 @@
-/*
-Problem
-
-You are given an array prices where prices[i] is the price of a given stock on day i.
-
-You can complete at most two transactions (a buy and a sell count as one transaction). You cannot hold more than one stock at a time.
-
-Return the maximum profit you can achieve.
-
-Approach
-
-We can use Dynamic Programming with states.
-
-We define four variables to track maximum profit in each state:
-
-firstBuy – Maximum profit after buying the first stock (negative because we spent money).
-
-firstSell – Maximum profit after selling the first stock.
-
-secondBuy – Maximum profit after buying the second stock (taking profit from firstSell into account).
-
-secondSell – Maximum profit after selling the second stock.
-
-We iterate over each price and update these states as we go:
-
-firstBuy   = max(firstBuy, -price)
-firstSell  = max(firstSell, firstBuy + price)
-secondBuy  = max(secondBuy, firstSell - price)
-secondSell = max(secondSell, secondBuy + price)
-
-
-firstBuy → we want the lowest price for first buy.
-
-firstSell → we want to maximize profit from firstSell.
-
-secondBuy → we “reinvest” profit from firstSell.
-
-secondSell → maximize total profit after second sell.
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int maxProfit(vector<int>& prices) {
-    int firstBuy = INT_MIN, firstSell = 0;
-    int secondBuy = INT_MIN, secondSell = 0;
-
-    for(int price : prices){
-        firstBuy = max(firstBuy, -price);
-        firstSell = max(firstSell, firstBuy + price);
-        secondBuy = max(secondBuy, firstSell - price);
-        secondSell = max(secondSell, secondBuy + price);
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int n = a.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=1;buy>=0;buy--)
+            {
+                for(int cap=1;cap<=2;cap++)
+                {
+                    if (buy) {
+                        // Either buy now, or skip
+                        dp[i][buy][cap] = max(-a[i] + dp[i+1][0][cap],dp[i+1][1][cap]);
+                    } else {
+                        // Either sell now, or skip
+                        dp[i][buy][cap] = max(a[i] +dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
-
-    return secondSell;
-}
-
-int main() {
-    vector<int> prices = {3,3,5,0,0,3,1,4};
-    cout << "Max Profit: " << maxProfit(prices) << endl;
-    return 0;
-}
+};
