db786253dcc69859a4f6e1a7097430b2af3f227e
purrvax <ppsingh_b24@et.vjti.ac.in>
2025-10-23 17:32:47 +0530

    N-queen added

--- 51.N-Queens.cpp
+++ 51.N-Queens.cpp
@@ -3,62 +3,42 @@ using namespace std;
 
 class Solution {
 public:
-    // Function to check if placing a queen at (row, col) is safe
-    bool isSafe(int row, int col, vector<string> &temp, int n) {
-        int duprow = row;
-        int dupcol = col;
-        // Check all columns to the left in the same row
-        for (int j = 0; j < col; j++) {
-            if (temp[row][j] == 'Q') return false;
-        }
-
-        // Check upper-left diagonal
-        while(row >= 0 && col >= 0){
-            if(temp[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-        row = duprow;
-        col = dupcol;
-        // Check lower-left diagonal
-        while(row < n && col >= 0){
-            if(temp[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-
-        return true;
-    }
-
-    // Backtracking function to place queens column by column
-    void solve(int col, vector<string> temp, vector<vector<string>> &ans, int n) {
-        // If all columns are filled, add current board to answer
+    void solve(int col, int n, vector<string> &board, 
+               vector<vector<string>> &ans, 
+               vector<int> &leftrow, vector<int> &upperdiag, vector<int> &lowerdiag) {
+        
         if (col == n) {
-            ans.push_back(temp);
+            ans.push_back(board);
             return;
         }
 
         for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, temp, n)) {
-                // Place queen
-                temp[row][col] = 'Q';
-                // Recurse for next column
-                solve(col + 1, temp, ans, n); 
-                // Backtrack and remove queen
-                temp[row][col] = '.';        
+            if (leftrow[row] == 0 && lowerdiag[row + col] == 0 && upperdiag[(n - 1) + (col - row)] == 0) {
+                
+                board[row][col] = 'Q';
+                leftrow[row] = 1;
+                lowerdiag[row + col] = 1;
+                upperdiag[(n - 1) + (col - row)] = 1;
+
+                solve(col + 1, n, board, ans, leftrow, upperdiag, lowerdiag);
+
+                board[row][col] = '.';  // backtrack
+                leftrow[row] = 0;
+                lowerdiag[row + col] = 0;
+                upperdiag[(n - 1) + (col - row)] = 0;
             }
         }
     }
 
-    // Main function to call backtracking
     vector<vector<string>> solveNQueens(int n) {
-    vector<vector<string>> ans;
-    vector<string> temp;
-    string s(n, '.');
-    for(int i = 0 ; i < n ; i++) temp.push_back(s); // FIXED
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
 
-    solve(0, temp, ans, n);
-    return ans;
-}
+        vector<int> leftrow(n, 0);
+        vector<int> lowerdiag(2 * n - 1, 0);
+        vector<int> upperdiag(2 * n - 1, 0);
 
+        solve(0, n, board, ans, leftrow, upperdiag, lowerdiag);
+        return ans;
+    }
 };
